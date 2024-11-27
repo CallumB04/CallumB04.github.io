e626a1343485c377760dbb02b908a252ba02a8b7
CallumB04 <callumburgoyne04@gmail.com>
2024-11-27 19:41:53 +0000

    feat: add home section title

--- src/pages/PortfolioPage/sections/HomeSection/HomeSection.jsx
+++ src/pages/PortfolioPage/sections/HomeSection/HomeSection.jsx
@@ -4,7 +4,9 @@ const HomeSection = ({ sectionRef }) => {
     
     return (
         <section className='portfolio-section light-mode' id='home-section' ref={sectionRef}>
-                                
+            <div id='home-section-content'>
+                <h1 className='section-title' id='home-section-title'>Hi, I'm Callum Burgoyne 👋</h1>          
+            </div>
         </section>
     );
 };
