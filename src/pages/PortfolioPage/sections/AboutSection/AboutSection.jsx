a31000ad14b5a1f4758dae46e7054369d43acb55
CallumB04 <callumburgoyne04@gmail.com>
2024-11-28 22:22:49 +0000

    feat: add title and description to about section

--- src/pages/PortfolioPage/sections/AboutSection/AboutSection.jsx
+++ src/pages/PortfolioPage/sections/AboutSection/AboutSection.jsx
@@ -4,7 +4,16 @@ const AboutSection = ({ sectionRef }) => {
     
     return (
         <section className='portfolio-section dark-mode' id='about-section' ref={sectionRef}>
-                                
+            <h2 className='section-title about-section-title'>About me</h2>
+            <p className='section-description' id='about-section-description'>
+                Hi, I'm Callum, a 20 year old self-taught Software Developer 
+                from <strong>Kent, United Kingdom</strong>. I'm passionate about creating 
+                efficient software, with experience in <strong>Web Development</strong>
+                , <strong>Game Development</strong>, and <strong>Backend Systems</strong>. 
+                I'm currently seeking a <strong>Level 4 Apprenticeship</strong> in any 
+                area of Software Development, and eager to bring my skills and passion 
+                for learning to a collaborative work environment!
+            </p>       
         </section>
     );
 };
