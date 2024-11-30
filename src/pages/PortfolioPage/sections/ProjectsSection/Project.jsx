20bce0ccbb1378a2e139c8fb2ce40b77ad6e38ef
CallumB04 <callumburgoyne04@gmail.com>
2024-11-30 14:53:20 +0000

    feat: add project hover content

--- src/pages/PortfolioPage/sections/ProjectsSection/Project.jsx
+++ src/pages/PortfolioPage/sections/ProjectsSection/Project.jsx
@@ -1,10 +1,49 @@
 import './Project.css';
+import { useEffect, useRef } from 'react';
 
 const Project = () => {
 
+    const projectRef = useRef(null); // project
+    const projectDarkenRef = useRef(null); // background to fade over image when text appears
+    const projectContentRef = useRef(null); // title, description and technologies
+
+    useEffect(() => {
+        let project = projectRef.current;
+        let background = projectDarkenRef.current;
+        let content = projectContentRef.current;
+
+        project.addEventListener("mouseover", () => {
+            background.style.opacity = "0.85";
+            content.style.opacity = "1";
+            content.style.transform = "translateY(0px)";
+        });
+
+        project.addEventListener("mouseout", () => {
+            background.style.opacity = "0";
+            content.style.opacity = "0";
+            content.style.transform = "translateY(20px)";
+        });
+    }, [])
+
     return (
-        <div className='projects-section-project'>
-            <img src='/assets/project-images/react-todolist.png' className='projects-section-project-image' />
+        <div className='projects-section-project' ref={projectRef}>
+            <img 
+                src='/assets/project-images/react-todolist.png' 
+                className='projects-section-project-image'
+            />
+
+            <div className='projects-section-project-darken' ref={projectDarkenRef}></div>
+
+            <div className='projects-section-project-content' ref={projectContentRef}>
+                <h3 className='projects-section-project-title'>React-Todolist</h3>
+                <p className='projects-section-project-description'>An interactive to-do list App</p>
+
+                <div className='projects-section-project-technologies'>
+                    <p className='projects-section-project-technology'>React</p>
+                    <p className='projects-section-project-technology'>HTML</p>
+                    <p className='projects-section-project-technology'>CSS</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/PortfolioPage/sections/ProjectsSection/ProjectsSection.jsx
+++ src/pages/PortfolioPage/sections/ProjectsSection/ProjectsSection.jsx
@@ -1,10 +1,18 @@
 import './ProjectsSection.css';
+import Project from './Project.jsx'
 
 const ProjectsSection = ({ sectionRef, projects }) => {
     
     return (
         <section className='portfolio-section light-mode' id='projects-section' ref={sectionRef}>
             <h2 className='section-title projects-section-title'>My Projects</h2>
+
+            <div id='projects-section-projects-wrapper'>
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+            </div>
         </section>
     );
 };
