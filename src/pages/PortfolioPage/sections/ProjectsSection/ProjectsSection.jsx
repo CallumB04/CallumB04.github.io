7bf8256991f771b2512d5ad4fdc8269fb91fe572
CallumB04 <callumburgoyne04@gmail.com>
2024-11-30 13:30:16 +0000

    feat: add projects section title

--- src/pages/PortfolioPage/sections/ProjectsSection/ProjectsSection.jsx
+++ src/pages/PortfolioPage/sections/ProjectsSection/ProjectsSection.jsx
@@ -1,10 +1,10 @@
 import './ProjectsSection.css';
 
-const ProjectsSection = ({ sectionRef }) => {
+const ProjectsSection = ({ sectionRef, projects }) => {
     
     return (
         <section className='portfolio-section light-mode' id='projects-section' ref={sectionRef}>
-                                
+            <h2 className='section-title projects-section-title'>My Projects</h2>
         </section>
     );
 };
