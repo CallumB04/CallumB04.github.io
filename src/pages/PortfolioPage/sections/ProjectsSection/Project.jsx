3f9944b2de5852ff9bea23c3e70a3723c82b02e8
CallumB04 <callumburgoyne04@gmail.com>
2024-11-30 13:56:07 +0000

    feat: add Project component

---
+++ src/pages/PortfolioPage/sections/ProjectsSection/Project.jsx
@@ -0,0 +1,12 @@
+import './Project.css';
+
+const Project = () => {
+
+    return (
+        <div className='projects-section-project'>
+            <img src='/assets/project-images/react-todolist.png' className='projects-section-project-image' />
+        </div>
+    )
+}
+
+export default Project;
